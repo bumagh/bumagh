3dd3515a2d71e3798890f5abb61e5699c4bee93e
bumagh <bumagh@users.noreply.github.com>
2024-02-17 03:10:18 +0000

    update

--- assets/doc2024_1.1王者荣耀经济模式.md.50260af7.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,o as a,c as t,Q as o}from"./chunks/framework.bf7c2cc3.js";const f=JSON.parse('{"title":"0 历史","description":"","frontmatter":{},"headers":[],"relativePath":"doc2024/1.1王者荣耀经济模式.md","filePath":"doc2024/1.1王者荣耀经济模式.md"}'),_={name:"doc2024/1.1王者荣耀经济模式.md"},r=o(`<h1 id="_0-历史" tabindex="-1">0 历史 <a class="header-anchor" href="#_0-历史" aria-label="Permalink to &quot;0 历史&quot;">​</a></h1><pre><code>王者荣耀游戏本身也是从端游移植过来的，起初也只是一个开源游戏，被某个工作室开发竞争上线被收购以后，投资优化，才成就了如今的王者荣耀。
-</code></pre><h1 id="_1-人员分析" tabindex="-1">1 人员分析 <a class="header-anchor" href="#_1-人员分析" aria-label="Permalink to &quot;1 人员分析&quot;">​</a></h1><pre><code>一个游戏工作室：开发者，美术，策划，运营
-其他：投资人，推广，美术，赛事等等
-此外还有玩家，职业，自媒体人
-</code></pre><h1 id="_2-收益分析" tabindex="-1">2 收益分析 <a class="header-anchor" href="#_2-收益分析" aria-label="Permalink to &quot;2 收益分析&quot;">​</a></h1><pre><code>从上到下：
-投资方，管理层，工作层
-职业，自媒体人，玩家
-</code></pre>`,6),c=[r];function n(d,s,i,h,l,p){return a(),t("div",null,c)}const u=e(_,[["render",n]]);export{f as __pageData,u as default};
